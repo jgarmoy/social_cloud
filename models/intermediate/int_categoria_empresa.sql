5fa9c07e33cf80081214f1832671bd593fffe6d7
Juan Manuel García Moyano <juanmanuelgarmoy@outlook.es>
2026-05-15 10:55:49 +0000

    Modelos para realizar el modelado relacional

--- models/intermediate/int_categoria_empresa.sql
+++ models/intermediate/int_categoria_empresa.sql
@@ -1,11 +1,14 @@
-with source as (
-    select * from {{ source('social_cloud_schema', 'raw_perfiles_empresas') }}
-),
-generar_surrogate_key as (
-    select distinct
-        {{ dbt_utils.generate_surrogate_key(["lower(coalesce(empresa_categoria, 'Desconocido'))"]) }} as id_categoria_empresa,
-        lower(empresa_categoria) as nombre
-    from source
-)
+with
+    source as (
+        select * from {{ ref("stg_social_cloud_schema__raw_perfiles_empresas") }}
+    ),
+    generar_surrogate_key as (
+        select distinct
+            {{ dbt_utils.generate_surrogate_key(["empresa_categoria"]) }}
+            as id_categoria_empresa,
+            empresa_categoria as nombre
+        from source
+    )
 
-select * from generar_surrogate_key
+select *
+from generar_surrogate_key
