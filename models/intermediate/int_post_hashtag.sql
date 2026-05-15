163bcd71e296f9a333e0d7fe91e8ef3bc601b871
Juan Manuel García Moyano <juanmanuelgarmoy@outlook.es>
2026-05-15 10:57:11 +0000

    Modelos para realizar el modelado relacional

--- models/intermediate/int_post_hashtag.sql
+++ models/intermediate/int_post_hashtag.sql
@@ -1,1 +1,11 @@
-with source 
+with
+    source as (select * from {{ ref("int_hashtag_post_separar") }}),
+    generar_surrogate_key as (
+        select distinct
+            {{ dbt_utils.generate_surrogate_key(["hashtag"]) }} as id_hashtag,
+            {{ dbt_utils.generate_surrogate_key(["post_id"]) }} as id_post
+        from source
+    )
+
+select *
+from generar_surrogate_key
