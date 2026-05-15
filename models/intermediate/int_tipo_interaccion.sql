99dc580808b2f47d3ef312de2dc4c019cf95f866
Juan Manuel García Moyano <juanmanuelgarmoy@outlook.es>
2026-05-15 10:57:34 +0000

    Modelos para realizar el modelado relacional

--- models/intermediate/int_tipo_interaccion.sql
+++ models/intermediate/int_tipo_interaccion.sql
@@ -1,10 +1,14 @@
-with source as (
-    select * from {{ source('social_cloud_schema', 'raw_posts_interacciones') }}
-),
-generar_surrogate_key as (
-    select distinct
-        {{ dbt_utils.generate_surrogate_key(['lower(tipo_interaccion)']) }} as id_tipo_interaccion,
-        lower(tipo_interaccion) as nombre
-    from source
-)
-select * from generar_surrogate_key
+with
+    source as (
+        select * from {{ ref("stg_social_cloud_schema__raw_posts_interacciones") }}
+    ),
+    generar_surrogate_key as (
+        select distinct
+            {{ dbt_utils.generate_surrogate_key(["tipo_interaccion"]) }}
+            as id_tipo_interaccion,
+            tipo_interaccion as nombre
+        from source
+    )
+
+select *
+from generar_surrogate_key
