9720762ad80f0254faad1868df9d0623b68ad995
Juan Manuel García Moyano <juanmanuelgarmoy@outlook.es>
2026-05-15 10:56:16 +0000

    Modelos para realizar el modelado relacional

--- models/intermediate/int_genero.sql
+++ models/intermediate/int_genero.sql
@@ -1,15 +1,12 @@
-with source as (
-    select * from {{ source('social_cloud_schema', 'raw_audiencia_demografica') }}
-),
-obtener_genero as (
-    select
-        coalesce(gender_segment)
-    from source
-),
-generar_surrogate_key as (
-    select
-        {{ dbt_utils.generate_surrogate_key(["lower(coalesce(genero, 'Desconocido'))"]) }} as id_genero,
-        lower(coalesce(genero, 'Desconocido')) as nombre
-    from unir_generos_tk_yt_ig
-)
-select * from generar_surrogate_key
+with
+    source as (select * from {{ ref("int_audiencia_demografica") }}),
+    
+    generar_surrogate_key as (
+        select distinct
+            {{ dbt_utils.generate_surrogate_key(["genero"]) }} as id_genero,
+            genero as nombre
+        from source
+    )
+
+select *
+from generar_surrogate_key
